ba67d9d9430d1303cc757ce32590f70b14251e12
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-03 12:02:13 +0200

    Brew formula update for saucectl-internal version v0.3.17

--- Formula/saucectl-internal.rb
+++ Formula/saucectl-internal.rb
@@ -2,16 +2,16 @@
 class SaucectlInternal < Formula
   desc "Homebrew Formulae to saucectl binaries"
   homepage "https://saucelabs.com"
-  version "0.3.16"
+  version "0.3.17"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/saucelabs/saucectl-internal/releases/download/v0.3.16/saucectl-internal_0.3.16_mac_64-bit.tar.gz"
-    sha256 "f5694c603184e014ffc6d4d6cfbf051ac7c176040299296075490f8ac43b34b7"
+    url "https://github.com/saucelabs/saucectl-internal/releases/download/v0.3.17/saucectl-internal_0.3.17_mac_64-bit.tar.gz"
+    sha256 "bdc9729d02f5e1fac19ecb8432616faea95c1b1cf39877df360f215a3b239679"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/saucelabs/saucectl-internal/releases/download/v0.3.16/saucectl-internal_0.3.16_linux_64-bit.tar.gz"
-      sha256 "10156483e459154596211d895ec6fe86e99c6c83b8b89657ec8beecefe04084a"
+      url "https://github.com/saucelabs/saucectl-internal/releases/download/v0.3.17/saucectl-internal_0.3.17_linux_64-bit.tar.gz"
+      sha256 "37aa317db97a7e7ce8a32a81b696baf5fdcadd710a1efc34a40eb6d23928fd41"
     end
   end
 
